978ef2ce172045ca67da406c586fba838d9d280e
B Sanjeev Reddy <sanjeevteja143@gmail.com>
2022-09-23 04:21:16 +0530

    Distance between two places

---
+++ Main.js
@@ -0,0 +1,72 @@
+//javascript.js
+//set map options
+var myLatLng = { lat: 19.075983, lng: 72.877655 };
+var mapOptions = {
+    center: myLatLng,
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+};
+
+//create map
+var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
+//create a DirectionsService object to use the route method and get a result for our request
+var directionsService = new google.maps.DirectionsService();
+
+//create a DirectionsRenderer object which we will use to display the route
+var directionsDisplay = new google.maps.DirectionsRenderer();
+
+//bind the DirectionsRenderer to the map
+directionsDisplay.setMap(map);
+
+
+//define calcRoute function
+function calcRoute() {
+    //create request
+    var request = {
+        origin: document.getElementById("from").value,
+        destination: document.getElementById("to").value,
+        travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    }
+
+    //pass the request to the route method
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+
+            //Get distance and time
+            // India system hi-IN
+            const distance = parseInt(result.routes[0].legs[0].distance.value / 1000).toLocaleString('hi-IN');
+
+
+            const output = document.querySelector('#output');
+            output.innerHTML = "<div class='alert-info'>The distance between <b>" + document.getElementById("from").value + "</b> and <b>" + document.getElementById("to").value + "</b> is <b>" + distance + " " + "kms.<br /> Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + "</div>";
+            document.querySelector("#distval").innerHTML = "<div>" + distance + "kms</div>";
+            //display route
+            directionsDisplay.setDirections(result);
+        } else {
+            //delete route from map
+            directionsDisplay.setDirections({ routes: [] });
+            //center map in London
+            map.setCenter(myLatLng);
+
+            //show error message
+            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
+        }
+    });
+
+}
+
+
+
+//create autocomplete objects for all inputs
+var options = {
+    types: ['(cities)']
+}
+
+var input1 = document.getElementById("from");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+
+var input2 = document.getElementById("to");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
